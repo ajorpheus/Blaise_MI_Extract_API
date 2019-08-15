757cfa0966791d36971627c0940a97be55acc3e9
Catalina Garcia <catalina.garcia.mail@gmail.com>
2019-08-15 09:48:07 +0100

    Adds comments to functions.py

--- blaise_mi_extract_api/functions.py
+++ blaise_mi_extract_api/functions.py
@@ -3,6 +3,8 @@ from blaise_mi_extract_api.models import CaseResponse, Survey, Case, FieldPeriod
 import json
 
 
+# Temporary function to get json request for Management Information.
+# The actual request will come from rabbitmq?
 def get_json():
     with open('examples/message.json', 'r') as f:
         message = json.load(f)
@@ -10,17 +12,23 @@ def get_json():
 
 
 def search_db():
-    info_request_spec = get_json()
 
+    info_request_spec = get_json()
     survey_tla = info_request_spec.get('tla')
     field_period = info_request_spec.get('field_period')
 
+    # Collect all cases (with or without response) with their survey three letter acronym (tla) and field period
     response = db.session.query(Case)\
         .outerjoin(CaseResponse, Case.id == CaseResponse.case_id)\
         .join(Survey, Case.survey_id == Survey.id)\
-        .join(FieldPeriod, Case.field_period_id == FieldPeriod.id)\
-        .filter(Survey.tla == survey_tla)\
-        .filter(FieldPeriod.stage == field_period)\
-        .all()
+        .join(FieldPeriod, Case.field_period_id == FieldPeriod.id)
 
+    # Filter by tla
+    response = response.filter(Survey.tla == survey_tla)
+
+    # Filter by field period
+    response = response.filter(FieldPeriod.stage == field_period)
+
+    response = response.all()
     return
+
