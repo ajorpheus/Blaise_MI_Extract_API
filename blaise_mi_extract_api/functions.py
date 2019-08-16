e6b73506e1c3d4ab0b6ad25a7662f41290507c1d
Catalina Garcia <catalina.garcia.mail@gmail.com>
2019-08-16 15:23:50 +0100

    Adds layout for new mi functions

--- blaise_mi_extract_api/functions.py
+++ blaise_mi_extract_api/functions.py
@@ -12,9 +12,9 @@ def get_json():
     return message
 
 
-def extract_data_from_db():
-    # Based on the survey and field period requested by json file, a table of all responses is returned (even if ones
-    # for which the response_data is empty)
+def query_management_info():
+    # Based on the survey and field period requested by json file, a query for matching responses is returned (even
+    # if ones for which the response_data is empty)
 
     info_request_spec = get_json()
     survey_tla = info_request_spec.get('tla')
@@ -32,38 +32,52 @@ def extract_data_from_db():
     # Filter by field period
     response = response.filter(FieldPeriod.stage == field_period)
 
-    response_data = response.all()
     return response
 
 
-def map_to_management_info(response):
-    case_list = response.all()
+def gather_management_info():
+    # Given a query, returns a dictionary based on mi_spec
+    return
+
+
+def remove_field_from_management_info():
+    # Given a dictionary with mi_spec and list of fields to remove, and updated dictionary is returned without those
+    # fields
+    return
+
+
+def map_to_management_info(mi_query):
+    # Assumes the query
+    case_list = mi_query.all()
 
     # Create dictionary with management information requirements, i.e. output fields required for a given instrument
     if len(case_list) != 0:
         management_info = db.session.query(Instrument.MI_spec) \
             .filter(Instrument.id == case_list[0].Case.instrument_id).first()
+    else:
+        return
 
-        # Dictionary with management information requirements
-        management_info = ast.literal_eval(management_info.MI_spec)
-        my_dict = {}
+    # Dictionary with management information requirements
+    management_info = ast.literal_eval(management_info.MI_spec)
+    my_dict = {}
 
-        for i, val in enumerate(case_list):
-            case_response_block = case_list[i].CaseResponse
+    for i, val in enumerate(case_list):
+        serial_number = case_list[i].Case.serial_number
+        case_response_block = case_list[i].CaseResponse
 
-            if case_response_block is None:
-                my_dict[i] = {key: 'NULL' for key in management_info.keys()}
-            else:
-                # Dictionary with all response_data for a given case
-                case_response_dict = ast.literal_eval(case_response_block.response_data)
+        if case_response_block is None:
+            my_dict[serial_number] = {key: 'NULL' for key in management_info.keys()}
+        else:
+            # Dictionary with all response_data for a given case
+            case_response_dict = ast.literal_eval(case_response_block.response_data)
 
-                # Find keys in case_response_dict which match values in management_info
-                # Create {management_info key : case_response_dict value}
-                my_dict[i] = {key: case_response_dict[management_info[key]] for key in management_info.keys()}
+            # Find keys in case_response_dict which match values in management_info
+            # Create {management_info key : case_response_dict value}
+            my_dict[serial_number] = {key: case_response_dict[management_info[key]] for key in management_info.keys()}
 
-        # Output dictionary as json
-        with open('test.json', 'w') as f:
-            json.dump(my_dict, f)
+    # Output dictionary as json
+    with open('test.json', 'w') as f:
+        json.dump(my_dict, f)
 
     return
 
--- blaise_mi_extract_api/views.py
+++ blaise_mi_extract_api/views.py
@@ -2,7 +2,7 @@ from flask import Blueprint, render_template, request, url_for
 from flask_login import login_required
 from blaise_mi_extract_api.models import ApiKey
 from blaise_mi_extract_api.extensions import login_manager
-from blaise_mi_extract_api.functions import extract_data_from_db, map_to_management_info
+from blaise_mi_extract_api.functions import query_management_info, map_to_management_info
 import base64
 
 
@@ -13,8 +13,8 @@ api_view = Blueprint('api_views', __name__, url_prefix="/", template_folder='tem
 @login_required
 def example_route():
     name = request.args.get('name', '')
-    data = extract_data_from_db()
-    managment_info = map_to_management_info(data)
+    mi_query = query_management_info()
+    managment_info = map_to_management_info(mi_query)
     return 'Hello ' + name
 
 
