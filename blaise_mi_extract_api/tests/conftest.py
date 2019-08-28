e94a79318994ed01d3533448fa11d67e5a5b9051
Catalina Garcia <catalina.garcia.mail@gmail.com>
2019-08-28 11:06:37 +0100

    Refactors test fixtures and tests to avoid duplicating code

--- blaise_mi_extract_api/tests/conftest.py
+++ blaise_mi_extract_api/tests/conftest.py
@@ -25,9 +25,7 @@ def client(request):
 
 
 @pytest.fixture()
-def add_response_to_db():
-    # app = create_app('settings.TestConfig')
-
+def add_response_to_db(request):
     api_key = ApiKey()
     api_key.api_key = '123456'
     db.session.add(api_key)
@@ -39,58 +37,10 @@ def add_response_to_db():
 
     instrument = Instrument()
     instrument.id = 1
-    instrument.MI_spec = '{"serial_number": "QID.Case_ID"}'
-    instrument.survey_id = survey.id
-    instrument.field_period_id = 1
-    db.session.add(instrument)
-
-    case = Case()
-    case.id = 1
-    case.survey_id = survey.id
-    case.instrument_id = instrument.id
-    case.primary_key = 1234
-    case.field_period_id = 1
-    case.sample_id = 5
-    case.household = '2'
-    case.interviewer_id = ''
-    case.outcome_code = 10
-    case.phase = 'Live'
-    db.session.add(case)
-
-    case_response = CaseResponse()
-    case_response.case_id = case.id
-    db.session.add(case_response)
-
-    field_period = FieldPeriod()
-    field_period.id = case.field_period_id
-    field_period.stage = '2001'
-    db.session.add(field_period)
-
-    sample = Sample()
-    sample.id = case.sample_id
-    sample.quota = '7'
-    sample.addresno = ''
-    db.session.add(sample)
-
-    db.session.commit()
-
-    return
-
-
-@pytest.fixture()
-def add_response_to_db_no_mi():
-    api_key = ApiKey()
-    api_key.api_key = '123456'
-    db.session.add(api_key)
-
-    survey = Survey()
-    survey.id = 1
-    survey.tla = 'OPN'
-    db.session.add(survey)
-
-    instrument = Instrument()
-    instrument.id = 1
-    instrument.MI_spec = ''
+    if hasattr(request, 'param'):
+        instrument.MI_spec = request.param[0]
+    else:
+        instrument.MI_spec = ''
     instrument.survey_id = survey.id
     instrument.field_period_id = 1
     db.session.add(instrument)
--- blaise_mi_extract_api/tests/test_api.py
+++ blaise_mi_extract_api/tests/test_api.py
@@ -20,8 +20,17 @@ class TestURLs:
     def test_nonexistent_time_period(self, client):
         url_data = client.get('/management_information/cat/2001?api_key=123456')
         assert url_data.status_code == 404
-    
-    def test_output_fields_correct(self, client):
+
+    def test_output_from_undefined_mi_spec(self, client):
+        url_data = client.get('/management_information/OPN/2001?api_key=123456')
+        data = json.loads(url_data.data)
+        assert data['1234']['QUOTA'] == '7'
+        assert data['1234']['ADDRESS'] is None
+        assert data['1234']['HHOLD'] == 2
+        assert data['1234']['INTNUM'] == ''
+
+    @pytest.mark.parametrize('add_response_to_db', [['{"serial_number": "QID.Case_ID"}']], indirect=True)
+    def test_output_fields_correct(self, client, add_response_to_db):
         url_data = client.get('/management_information/OPN/2001?api_key=123456')
         data = json.loads(url_data.data)
         assert data['1234']['QUOTA'] == '7'
@@ -30,14 +39,4 @@ class TestURLs:
         assert data['1234']['INTNUM'] == ''
         assert data['1234']['serial_number'] is None
 
-
-@pytest.mark.usefixtures('client', 'add_response_to_db_no_mi')
-def test_output_from_undefined_mi_spec(client):
-    url_data = client.get('/management_information/OPN/2001?api_key=123456')
-    data = json.loads(url_data.data)
-    assert data['1234']['QUOTA'] == '7'
-    assert data['1234']['ADDRESS'] is None
-    assert data['1234']['HHOLD'] == 2
-    assert data['1234']['INTNUM'] == ''
-    # assert data['1234']['serial_number'] is None
 
