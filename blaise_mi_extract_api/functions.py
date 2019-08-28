fac430d81e8c6eeaddba69dc3da2175b4cf639ee
Catalina Garcia <catalina.garcia.mail@gmail.com>
2019-08-28 14:08:34 +0100

    Adds docstring for map_to_management_info

--- blaise_mi_extract_api/functions.py
+++ blaise_mi_extract_api/functions.py
@@ -27,7 +27,7 @@ def build_query_for_case_list(survey_tla, field_period, phase="Live"):
         .outerjoin(CaseResponse, Case.id == CaseResponse.case_id) \
         .outerjoin(Sample, Case.sample_id == Sample.id) \
         .join(Survey, Case.survey_id == Survey.id) \
-        .join(FieldPeriod, Case.field_period_id == FieldPeriod.id)\
+        .join(FieldPeriod, Case.field_period_id == FieldPeriod.id) \
         .filter(Survey.tla == survey_tla) \
         .filter(FieldPeriod.stage == field_period) \
         .filter(Case.phase == phase)
@@ -37,10 +37,11 @@ def build_query_for_case_list(survey_tla, field_period, phase="Live"):
 
 def gather_management_info_spec(instrument_id, database=None):
     """
-    Queries a database to obtain the management information specification associated with an instrument.
+    Queries a database to obtain the Management Information specification associated with an instrument.
 
     Parameters:
-        instrument_id -- the id for an instrument database -- Database containing the instrument
+        instrument_id -- the id for an instrument
+        database -- Database containing the instrument
     Returns:
         management_info -- Dictionary containing the management information specification. If the database is not
         specified, it returns an empty dictionary
@@ -63,6 +64,21 @@ def gather_management_info_spec(instrument_id, database=None):
 
 
 def map_to_management_info(survey_tla, field_period):
+    """
+    Obtains the default Management Information for each case, as well as any other requested by MI_spec (
+    gather_management_info_spec()). Each case is identified by a primary_key.
+
+    Parameters:
+        survey_tla -- Three letter acronym for a survey e.g. 'OPN'
+        field_period -- Format yymm e.g. '1901'
+    Returns:
+        json.dumps(management_info_out) -- JSON formatted str containing the management information for each case
+
+    Example output:
+        If a single case were found, the output would be:
+        {"1234": {"QUOTA":"1", "ADDRESS":"2", "HHOLD":"1", "INTNUM": "123"}}
+        where 1234 is the primary_key.
+    """
 
     case_list = build_query_for_case_list(survey_tla, field_period).all()
 
@@ -82,9 +98,9 @@ def map_to_management_info(survey_tla, field_period):
         # Default output for cases (following CMS
         # https://collaborate2.ons.gov.uk/confluence/display/QSS/Blaise+5+Management+Information+%28MI%29+Extract )
         management_info_out[primary_key] = {"QUOTA": row.quota,
-                                              "ADDRESS": row.addressno,
-                                              "HHOLD": row.Case.household,
-                                              "INTNUM": row.Case.interviewer_id}
+                                            "ADDRESS": row.addressno,
+                                            "HHOLD": row.Case.household,
+                                            "INTNUM": row.Case.interviewer_id}
 
         if case_response_block is None or case_response_block.response_data is None:
             management_info_out[primary_key].update({key: None for key in management_info_spec.keys()})
@@ -95,6 +111,6 @@ def map_to_management_info(survey_tla, field_period):
             # Find keys in case_response_dict which match values in management_info
             # Create {management_info key : case_response_dict value}
             management_info_out[primary_key].update({key: case_response_dict[management_info_spec[key]]
-                                                       for key in management_info_spec.keys()})
+                                                     for key in management_info_spec.keys()})
 
     return json.dumps(management_info_out)
